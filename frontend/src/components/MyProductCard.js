f561fb9687a920221375cba395471b132af186aa
IMS <gudwnsrh@gmail.com>
2022-03-31 19:12:35 +0900

    22.03.31

--- frontend/src/components/MyProductCard.js
+++ frontend/src/components/MyProductCard.js
@@ -68,7 +68,9 @@ const MyProductCard = ({
     if (!productTokenId) return;
 
     axios
-      .get("/user/picture", {
+    // http://j6a507.p.ssafy.io:8080/
+    // /user/picture
+      .get("http://j6a507.p.ssafy.io:8080/user/picture", {
         params: {
           id: productTokenId,
         },
