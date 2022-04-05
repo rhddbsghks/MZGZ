bd341cd08c8a692eabcea9e4476a8639a69370dd
gyh8781 <gyh8781@gmail.com>
2022-04-05 15:59:37 +0900

    feat: 로고 수정

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -9,24 +9,38 @@ const Layout = ({ children, account }) => {
         templateColumns="repeat(12, 1fr)"
         bg="unset"
         boxShadow="lg"
-        p={4}
         justifyContent="space-around"
         alignItems="center"
+        h="80px"
       >
         <GridItem colSpan={1} />
         <GridItem colSpan={1} justifySelf="center">
           <Link to="/">
-            <Image src="/logo.png" alt="img" w={18} h={18}></Image>
+            <Image src="/logo.png" alt="img" h="50px"></Image>
           </Link>
         </GridItem>
         <GridItem colSpan={5}>
-          {account ? <Text><Link to="/my-product"><strong>{account}</strong></Link>님 환영합니다.</Text> : <Text><strong>로그인 해주세요.</strong></Text>}
+          {account ? (
+            <Text>
+              <Link to="/my-product">
+                <strong>{account}</strong>
+              </Link>
+              님 환영합니다.
+            </Text>
+          ) : (
+            <Text>
+              <strong>로그인 해주세요.</strong>
+            </Text>
+          )}
         </GridItem>
         <GridItem colSpan={1} justifySelf="center">
           <Link
             to="/add-product"
-            size="sm" colorScheme="blue"
-            textColor="blue" fontWeight="bold" fontSize="large"
+            size="sm"
+            colorScheme="blue"
+            textColor="blue"
+            fontWeight="bold"
+            fontSize="large"
           >
             상품 등록
           </Link>
@@ -34,8 +48,11 @@ const Layout = ({ children, account }) => {
         <GridItem colSpan={1} justifySelf="center">
           <Link
             to="/my-product"
-            size="sm" colorScheme="blue"
-            textColor="blue" fontWeight="bold" fontSize="large"
+            size="sm"
+            colorScheme="blue"
+            textColor="blue"
+            fontWeight="bold"
+            fontSize="large"
           >
             내 상품
           </Link>
@@ -43,16 +60,22 @@ const Layout = ({ children, account }) => {
         <GridItem colSpan={1} justifySelf="center">
           <Link
             to="/sale-product"
-            size="sm" colorScheme="blue"
-            textColor="blue" fontWeight="bold" fontSize="large"
-            >
-              판매 목록
+            size="sm"
+            colorScheme="blue"
+            textColor="blue"
+            fontWeight="bold"
+            fontSize="large"
+          >
+            판매 목록
           </Link>
         </GridItem>
         <GridItem colSpan={1} />
       </Grid>
       <Flex
-        direction="column" h="full" justifyContent="center" alignItems="center"
+        direction="column"
+        h="full"
+        justifyContent="center"
+        alignItems="center"
       >
         {children}
       </Flex>
