b5ec671a75e8f4bf2ad27d56d17eb1cec2278a94
강동석 <kds2048@naver.com>
2022-04-06 14:51:19 +0900

    22.04.06 modified index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,129 +10,128 @@ ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
-    <Particles
-      id="tsparticles"
-      options={{
-        background: {
-          color: {
-            value: "unset",
+      <Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "unset",
+            },
           },
-        },
-        particles: {
-          number: {
-            value: 80,
-            density: {
+          particles: {
+            number: {
+              value: 20,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ff0000",
+            },
+            shape: {
+              type: "image",
+              stroke: {
+                width: 0,
+                color: "#000000",
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+              image: {
+                src: "https://user-images.githubusercontent.com/70699213/161903715-45a6c685-fa60-43b1-b923-860c944508a4.png",
+                width: 100,
+                height: 100,
+              },
+            },
+            opacity: {
+              value: 0.5,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 3,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 20,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 20,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
               enable: true,
-              value_area: 800
-            }
-          },
-          color: {
-            value: "#ff0000"
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000"
+              distance: 150,
+              color: ["unset"],
+              opacity: 0.4,
+              width: 1,
             },
-            polygon: {
-              nb_sides: 5
-            },
-            image: {
-              src: "https://cdn.matteobruni.it/images/particles/github.svg",
-              width: 100,
-              height: 100
-            }
-          },
-          opacity: {
-            value: 0.5,
-            random: true,
-            anim: {
+            move: {
               enable: true,
-              speed: 3,
-              opacity_min: 0.1,
-              sync: false
-            }
+              speed: 2,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              attract: {
+                enable: false,
+              },
+            },
+            twinkle: {
+              particles: {
+                enable: true,
+                color: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
+                opacity: 1,
+              },
+            },
           },
-          size: {
-            value: 5,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 20,
-              size_min: 0.1,
-              sync: false
-            }
+          interactivity: {
+            detect_on: "window",
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 10,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 20,
+                size: 2,
+                duration: 2,
+                opacity: 0.8,
+                speed: 3,
+              },
+              repulse: {
+                distance: 100,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
           },
-          line_linked: {
-            enable: true,
-            distance: 150,
-            color: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
-            opacity: 0.4,
-            width: 1
-          },
-          move: {
-            enable: true,
-            speed: 2,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            attract: {
-              enable: false,
-            }
-          },
-          twinkle: {
-            particles: {
-              enable: true,
-              color: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
-              opacity: 1
-            },
-          }
-        },
-        interactivity: {
-          detect_on: "window",
-          events: {
-            onhover: {
-              enable: true,
-              mode: "repulse"
-            },
-            onclick: {
-              enable: true,
-              mode: "push"
-            },
-            resize: true
-          },
-          modes: {
-            grab: {
-              distance: 10,
-              line_linked: {
-                opacity: 1
-              }
-            },
-            bubble: {
-              distance: 20,
-              size: 2,
-              duration: 2,
-              opacity: 0.8,
-              speed: 3
-            },
-            repulse: {
-              distance: 100
-            },
-            push: {
-              particles_nb: 4
-            },
-            remove: {
-              particles_nb: 2
-            }
-          }
-        },
-        retina_detect: true,
-      }}
-    />
+          retina_detect: true,
+        }}
+      />
     </ChakraProvider>
-
   </React.StrictMode>,
   document.getElementById("root")
 );
